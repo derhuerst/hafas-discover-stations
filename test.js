5ba27355322706648ae449eeb2edcf58087934bb
Jannis R <mail@jannisr.de>
2018-01-16 23:05:34 +0100

    cheap test :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,29 @@
 'use strict'
 
-const test = require('tape')
+const vbb = require('vbb-hafas')
+const assert = require('assert')
 
 const createWalk = require('.')
 
-test('todo', (t) => {
-})
+const walk = createWalk(vbb)
+const friedrichstr = '900000100001'
+
+const data = walk(friedrichstr)
+data.on('error', assert.ifError)
+
+let stations = 0
+const knownStations = Object.create(null)
+
+const onData = (station) => {
+	assert.ok(station)
+	assert.strictEqual(station.type, 'station')
+	if (knownStations[station.id]) assert.fail(station.id + ' occured twice')
+
+	knownStations[station.id] = true
+	stations++
+	if (stations >= 50) {
+		data.removeListener('data', onData)
+		data.stop()
+	}
+}
+data.on('data', onData)
