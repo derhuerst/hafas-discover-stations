d9416d160aed6d248bd42848486e3d61da7d79c8
Jannis R <mail@jannisr.de>
2018-07-01 15:12:42 +0200

    hafas-client as peer dep, 2.2.0

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ const createWalk = require('.')
 const walk = createWalk(vbb)
 
 const friedrichstr = '900000100001'
-const stations = walk(friedrichstr)
+const stations = walk(friedrichstr, {concurrency: 8})
 
 stations
 .on('data', console.log)
