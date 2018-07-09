f7de6d73c08e0f131bd8128dd5935d43d015875e
Jannis R <mail@jannisr.de>
2018-07-09 10:49:01 +0200

    demo: write ndjson to stdout

--- example.js
+++ example.js
@@ -1,14 +1,29 @@
 'use strict'
 
 const vbb = require('vbb-hafas')
+const pump = require('pump')
+const through = require('through2')
+const ndjson = require('ndjson')
 
 const createWalk = require('.')
 
 const walk = createWalk(vbb)
-
 const friedrichstr = '900000100001'
 const stations = walk(friedrichstr, {concurrency: 8})
 
-stations
-.on('data', console.log)
-.on('error', console.error)
+const DEV = process.env.NODE_ENV === 'dev'
+stations.on('hafas-error', (err) => {
+	if (DEV) console.error(err)
+	else console.error(err && err.message || (err + ''))
+})
+
+pump(
+	stations,
+	through.obj((s, _, cb) => cb(null, [s.id, s.name])),
+	ndjson.stringify(),
+	process.stdout,
+	(err) => {
+		console.error(err)
+		process.exit(1)
+	}
+)
